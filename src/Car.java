ceb88f9715d9e424c36a4c641b4d8f1ce6b52aab
Diogordo08 <diogoaugustoleite@gmail.com>
2025-04-13 23:41:20 -0300

    "I put more things on it"

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
+
 public class Car {
     public String model;
     public String brand;
     public int year;
     public String color;
+    boolean engine = false;
 
-    void turnOnEngine(String model){
-        System.out.println(model + " engine is on");
+    void turnOnOffEngine(String model){
+        if(engine){
+            engine = false;
+            System.out.println("Your " + model + " engine is off");
+        }
+        else{
+            engine = true;
+            System.out.println("Your " + model + " engine is on");
+            System.out.println("Vruum vruum*");
+        }
+    }
+
+    void createNewCar(Scanner scan){
+        scan.nextLine(); // Clear the last line, 'cause nextInt auto adds a \n after it
+
+        System.out.println("Car model:");
+        model = scan.nextLine();
+
+        System.out.println("Car brand:");
+        brand = scan.nextLine();
+
+        System.out.println("Car year:");
+        year = scan.nextInt();
+        scan.nextLine(); // Again
+
+        System.out.println("Car color:");
+        color = scan.nextLine();
+
+        engine = false;
+    }
+
+    void info() {
+        System.out.println("Your car model is: " + model);
+        System.out.println("Your car brand is: " + brand);
+        System.out.println("Your car year is: " + year);
+        System.out.println("Your car color is: " + color);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanf = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         Car newCar = new Car();
 
         System.out.println("Car manager 2000");
@@ -9,39 +10,37 @@ public class Main {
         boolean exit = false;
         do
         {
+            System.out.println("---");
             System.out.println("Menu");
-            System.out.println("1- Create new car");
-            System.out.println("2- Start engine");
-            System.out.println("3- Close");
-            int choose = scanf.nextInt();
+            System.out.println("1- Add car");
+            System.out.println("2- On/Off engine");
+            System.out.println("3- My car info");
+            System.out.println("4- Close");
+            int choose = scan.nextInt();
             switch (choose){
                 case 1:
-                    scanf.nextLine(); // Clear the last line, 'cause nextInt auto adds a \n after it
-
-                    System.out.println("Car model:");
-                    newCar.model = scanf.nextLine();
-
-                    System.out.println("Car brand:");
-                    newCar.brand = scanf.nextLine();
-
-                    System.out.println("Car year:");
-                    newCar.year= scanf.nextInt();
-                    scanf.nextLine(); // Again
-
-                    System.out.println("Car color:");
-                    newCar.color = scanf.nextLine();
+                    newCar.createNewCar(scan);
                 break;
 
                 case 2:
-                    if(newCar.model.isEmpty()){
+                    if(newCar.model == null){
                         System.out.println("You don't have a car");
                     }
                     else{
-                        newCar.turnOnEngine(newCar.model);
+                        newCar.turnOnOffEngine(newCar.model);
                     }
                 break;
 
                 case 3:
+                    if(newCar.model == null){
+                        System.out.println("You don´t have a car");
+                    }
+                    else{
+                        newCar.info();
+                    }
+                break;
+
+                case 4:
                     System.out.println("Closing...");
                     exit = true;
                 break;
